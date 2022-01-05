6edbe6ecdaa85905450b7e8829842ef0a0672d0f
bbosuni1 <bbosuni1@naver.com>
2022-01-05 18:05:08 +0900

    1. Kafka Config 관련 수정  - ConsumerConfig 수정

--- src/main/kotlin/com/example/chatting/config/KafkaConsumerConfig.kt
+++ src/main/kotlin/com/example/chatting/config/KafkaConsumerConfig.kt
@@ -29,7 +29,7 @@ class KafkaConsumerConfig {
         mapOf(
             ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServer,
             ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
-            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to JsonDeserializer::class,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to JsonDeserializer::class.java,
             ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
         )
 }
