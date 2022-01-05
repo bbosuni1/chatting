915503a5d627c5526a516694f1396a02b2c4c10b
bbosuni1 <bbosuni1@naver.com>
2022-01-05 13:25:46 +0900

    1. import 정리

--- src/main/kotlin/com/example/chatting/chat/controller/ChatController.kt
+++ src/main/kotlin/com/example/chatting/chat/controller/ChatController.kt
@@ -4,9 +4,6 @@ import com.example.chatting.chat.domain.Message
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.core.KafkaTemplate
-import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.messaging.handler.annotation.Payload
-import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
