e45a222514eb4d11e5c24394f1c219cc60595144
bbosuni1 <bbosuni1@naver.com>
2022-01-05 17:08:01 +0900

    1. Kafka Config 관련 수정  - AutoConfiguration => 직접 설정 2. WebSocket Config 수정

---
+++ src/main/kotlin/com/example/chatting/config/KafkaConsumerConfig.kt
@@ -0,0 +1,35 @@
+package com.example.chatting.config
+
+import com.example.chatting.chat.domain.Message
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.support.serializer.JsonDeserializer
+
+@EnableKafka
+@Configuration
+class KafkaConsumerConfig {
+
+    @Value("\${kafka.bootstrap-server}")
+    lateinit var bootstrapServer: String
+
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Message> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Message>()
+        factory.consumerFactory = DefaultKafkaConsumerFactory(getConfig())
+        return factory
+    }
+
+    private fun getConfig(): Map<String, Any> =
+        mapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServer,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to JsonDeserializer::class,
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
+        )
+}
